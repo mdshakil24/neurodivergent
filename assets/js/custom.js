4aa5d208e5c0c0db4a6f357d0ce3989df2a2647a
mdshakil24 <shakilshd23@gmail.com>
2024-06-29 00:56:27 +0600

    Header, Footer, Slider Done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,4 +9,24 @@ $(document).ready(function(){
         $('.lv-header-nav-menu ul').slideToggle();
     });
 
+    $('.health-crisis-slider-content').owlCarousel({
+        items: 4,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        navText: ['<img src="assets/images/icons/arrow-left.png">',
+                  '<img src="assets/images/icons/arrow-right.png">'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            992: {
+                items: 4
+            }
+        }
+    });
+
+
 });
+
+
