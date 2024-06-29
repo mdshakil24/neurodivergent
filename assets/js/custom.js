3442f60ee3ca3b65a9449ea56d676aaab0a6e4b6
mdshakil24 <shakilshd23@gmail.com>
2024-06-29 22:10:17 +0600

    Home desktop completed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
         $('.lv-header-nav-menu ul').slideToggle();
     });
 
-    $('.health-crisis-slider-content').owlCarousel({
+    $('.nt-health-crisis-content').owlCarousel({
         items: 4,
         nav: true,
         dots: false,
@@ -30,3 +30,31 @@ $(document).ready(function(){
 });
 
 
+
+
+function mentalHealthTab(evt,tabId) {
+    let i;
+    let tabcontent;
+    let tabBtn;
+
+     // Get all elements with class="nt-mhu-right-text" and hide them
+     tabcontent = document.getElementsByClassName('nt-mhu-right-text');
+     for(i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = 'none';
+     }
+
+    // Get all elements with class="tab-btn" and remove the class "active"
+     tabBtn = document.getElementsByClassName('tab-btn');
+     for(i =0; i < tabBtn.length; i ++) {
+        tabBtn[i].className = tabBtn[i].className.replace("active","");
+     }
+
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabId).style.display = 'block';
+    evt.currentTarget.className += " active";
+
+
+}
+
+// mentalHealthTab()
